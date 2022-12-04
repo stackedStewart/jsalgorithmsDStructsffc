45cd4c94f44b92b70cd59d609151a3461995cf2f
John Stewart <jstewartstack@gmail.com>
2022-12-04 15:19:02 +0000

    boolfuncsfinish

---
+++ boolfuncs.js
@@ -0,0 +1,14 @@
+function isLess(a, b) {
+    // Only change code below this line
+    return a < b;
+    // Only change code above this line
+  }
+  
+  console.log(isLess(10, 15));
+  
+  /*
+  freecodecamp.org task
+  returning boolean values from functions
+  stewartStack github repo
+  12/04/2022
+  */
