3ff75a85728c420f51fd12dd4246df80d741228b
John Stewart <jstewartstack@gmail.com>
2022-12-10 09:40:31 +0000

    done

---
+++ objDotNot.js
@@ -0,0 +1,12 @@
+// Setup
+const testObj = {
+     "hat": "ballcap",
+     "shirt": "jersey",
+     "shoes": "cleats"
+   };
+   
+   // Only change code below this line
+   const hatValue = testObj.hat;
+   console.log(testObj["hat"]);      
+   const shirtValue = testObj.shirt;
+   console.log(testObj["shirt"]);   
