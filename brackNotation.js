1c4d8a3c5f80f5f43cacbdd885c8b00643506905
John Stewart <jstewartstack@gmail.com>
2022-12-11 10:50:48 +0000

    finished

---
+++ brackNotation.js
@@ -0,0 +1,12 @@
+// Setup
+const testObj = {
+     "an entree": "hamburger",
+     "my side": "veggies",
+     "the drink": "water"
+   };
+   
+   // Only change code below this line
+   let entreeValue = testObj["an entree"];
+   console.log(entreeValue);   // Change this line
+   let drinkValue = testObj["the drink"];
+   console.log(drinkValue);    // Change this line
