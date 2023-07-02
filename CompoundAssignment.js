bc7e7ead5fa6dfc19106684e3d3c6c19f439295c
John Stewart <jstewartstack@gmail.com>
2023-07-02 22:18:22 +0100

    added -=

--- CompoundAssignment.js
+++ CompoundAssignment.js
@@ -1,4 +1,4 @@
-// Using a compound assignment
+// Using a compound assignment +=
 let a = 4;
 let b = 27;
 let c = 56;
@@ -7,4 +7,16 @@ a += 12;
 b += 52;
 c += 15;
 
-console.log(a, b, c);
+console.log("This is with += " + " " + a, b, c);
+
+// Now wit -=
+
+let d = 14
+let e = 25;
+let f = 82;
+
+d -= 65;
+e -= 12;
+f -= 55;
+
+console.log("This is with -= " + d, e, f);
