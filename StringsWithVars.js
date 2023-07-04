65399a76fe8f2a4e44db0bfb0181ce4e9bded90c
John Stewart <jstewartstack@gmail.com>
2023-07-04 15:24:53 +0100

    appending vars to strings

--- StringsWithVars.js
+++ StringsWithVars.js
@@ -4,3 +4,11 @@ const myName = "stackedStewart";
 const theStr = "Hello, my name is " + myName + " . I am a developer!"
 
 console.log(theStr)
+
+// Appending Vars to Strings
+const someAdjective = "amazing"
+let myStr = "Learning to code is "
+
+myStr += someAdjective;
+
+console.log(myStr)
