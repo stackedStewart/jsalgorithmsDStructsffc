37059fa52fa890dd8f53407c930b0292e66b170f
John Stewart <jstewartstack@gmail.com>
2023-07-04 14:01:41 +0100

    finished

--- SingleQuotes.js
+++ SingleQuotes.js
@@ -2,3 +2,7 @@
 
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 console.log(myStr)
+
+// More escape characters
+const myStr2 = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStr2);
