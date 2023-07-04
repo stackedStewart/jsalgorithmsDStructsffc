52b59f93d6f2c986c37d87a76bf57b19e25b2364
John Stewart <jstewartstack@gmail.com>
2023-07-04 14:04:06 +0100

    concate added

--- SingleQuotes.js
+++ SingleQuotes.js
@@ -6,3 +6,7 @@ console.log(myStr)
 // More escape characters
 const myStr2 = "FirstLine\n\t\\SecondLine\nThirdLine";
 console.log(myStr2);
+
+// Concatenating
+const myConcat = "This is the start. " + "This is the end.";
+console.log(myConcat) 
