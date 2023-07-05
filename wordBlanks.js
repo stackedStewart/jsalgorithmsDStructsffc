04ec73a8ad83c38df0364ba538a5e593cb4d9619
John Stewart <jstewartstack@gmail.com>
2023-07-05 18:40:14 +0100

    using vars to fill in blanks

---
+++ wordBlanks.js
@@ -0,0 +1,18 @@
+// Using variables to fill in blanks
+const myNoun = "ball";
+const myAdjective = "big";
+const myVerb = "bounced";
+const myAdverb = "about";
+
+const wordBlanks =
+  "I have a very " +
+  myAdjective +
+  " " +
+  myNoun +
+  ". It " +
+  myVerb +
+  " " +
+  myAdverb +
+  " everywhere.";
+
+console.log(wordBlanks);
