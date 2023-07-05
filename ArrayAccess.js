8f1ee363bdf825f16a77eeb9d1143468aed31c21
John Stewart <jstewartstack@gmail.com>
2023-07-05 18:55:33 +0100

    modifying te data with indexes

--- ArrayAccess.js
+++ ArrayAccess.js
@@ -1,5 +1,9 @@
 // Simple accessing arrays
 const myArray = [50, 60, 70];
-
+console.log(myArray);
 const myData = myArray[myArray.length - 1];
 console.log(myData);
+
+// Modifying the data with indexes
+myArray[0] = 45;
+console.log(myArray);
