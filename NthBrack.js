86fea026af2cf41551cbdb1d3edc66649a48229f
John Stewart <jstewartstack@gmail.com>
2023-07-05 18:22:30 +0100

    last letter with brcket notation

--- NthBrack.js
+++ NthBrack.js
@@ -4,3 +4,8 @@ const lastName = 'Lovelace';
 const thirdLetterOfLastName = lastName[2];
 
 console.log(thirdLetterOfLastName);
+
+// Last letter of lastName
+
+const lastLetterOfLastName = lastName[lastName.length - 1];
+console.log(lastLetterOfLastName);
