00905335ebbee2edcdbfc1034bd5143853c97fff
John Stewart <jstewartstack@gmail.com>
2023-07-06 22:27:45 +0100

    function with arguments

---
+++ functionWithArgs.js
@@ -0,0 +1,8 @@
+// Doing function with Arguments converting them into a sum when called
+
+function functionWithArgs(hello, world) {
+     console.log(hello + world)
+}
+
+functionWithArgs(1, 2);
+functionWithArgs(7, 9);
