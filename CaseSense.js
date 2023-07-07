e01409429f4d6a078da7b09cac6ab1f5636cf85a
John Stewart <jstewartstack@gmail.com>
2023-07-07 21:20:11 +0100

    how case sensetivity works.

---
+++ CaseSense.js
@@ -0,0 +1,14 @@
+// Case Sesitivity in variables
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+console.log(studlyCapVar, properCamelCase, titleCaseOver)
+
